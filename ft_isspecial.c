cf1cc2f1f323eff2b64dc6785b72fea6dd4f3ba9
jgo-42 <jingmuio@gmail.com>
2023-02-13 10:21:25 +0900

    :bug: Fix: ft_isspecial.c

--- ft_isspecial.c
+++ ft_isspecial.c
@@ -6,18 +6,17 @@
 /*   By: jgo <jgo@student.42seoul.fr>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/09 17:29:34 by jgo               #+#    #+#             */
-/*   Updated: 2023/02/09 17:37:20 by jgo              ###   ########.fr       */
+/*   Updated: 2023/02/13 10:19:56 by jgo              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int	ft_isspecial(int c)
+t_bool	ft_isspecial(int c)
 {
-	if (ft_isspace(c))
-		return (0);
 	if ((32 <= c && c <= 47) || (58 <= c && c <= 64) \
-		|| (91 <= c && c <= 96) || (123 <= c && c <= 126))
-		return (0);
-	return (1);
+		|| (91 <= c && c <= 96) || (123 <= c && c <= 126) \
+		|| ft_isspace(c))
+		return (FT_TRUE);
+	return (FT_FALSE);
 }
